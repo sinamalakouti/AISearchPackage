7547e66b9a8717d4cb80af9f16d68416d33c9cc1
sina <s.malakouti2015@aut.ac.ir>
2018-06-04 20:34:03 +0430

    creating the project

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        int a = 1;
+        getNumber(a);
+
+    }
+
+
+
+
+
+    private static void getNumber (int a){
+
+        out.println("hello");
+
+    }
+}
+
+
