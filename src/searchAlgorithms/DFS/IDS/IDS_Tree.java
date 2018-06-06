fcd7f0c46bc5d5e3aa354d55636f59b0512bb412
sina <s.malakouti2015@aut.ac.ir>
2018-06-06 17:03:21 +0430

    add IDS_Tree

---
+++ src/searchAlgorithms/DFS/IDS/IDS_Tree.java
@@ -0,0 +1,44 @@
+package searchAlgorithms.DFS.IDS;
+
+import agents.Agent;
+import agents.State;
+import problem.Problem;
+import problem.Solution;
+import searchAlgorithms.DFS.DLS.DLS_Tree;
+import searchAlgorithms.SearchAlgorithms;
+import tree.Node;
+
+import java.util.Objects;
+
+/**
+ * Created by sina on 6/6/18.
+ */
+public class IDS_Tree extends SearchAlgorithms implements Agent {
+    final  int maxLimitDepth = 20;
+
+
+    @Override
+    public Solution solve(Problem problem, State start) {
+
+        Node currentNode = new Node(start);
+
+        for (int i = 1 ; i <= maxLimitDepth ; i ++)
+        {
+            DLS_Tree dls = new DLS_Tree(i);
+            Object res =  dls.recursiveDLS(currentNode,problem,i);
+            if(res == null)
+                return  null;
+            else if ( res.getClass().isInstance(String.class) && ((String )res).compareTo("cutOff") == 0)
+                continue;
+            else
+                return (Solution) res;
+        }
+        return null;
+    }
+
+    @Override
+    public Object execute(Object p) {
+        return null;
+    }
+
+}
